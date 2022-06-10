82f21257775c2af88d59f4b1ad9d32181ad6403b
Devin Garcia <devingarcia5423@gmail.com>
2022-06-10 11:44:42 -0400

    completed core deliverables

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+import onfocus from 'react'
+import onblur from 'react'
+
+function getFocus() {
+  console.log('Hey! Eyes on me!')
+}
+
+function goodFocus() {
+  console.log('Good!')
+}
+
+function EyesOnMe() {
+  return (
+    <button onFocus={goodFocus} onBlur={getFocus}>Eyes on me</button>
+  )
+}
+
+export default EyesOnMe
